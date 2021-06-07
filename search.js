b21cb63a298fe7383e98fcf8e6cd210ecc138096
belalE <belalelsiesy@gmail.com>
2021-06-07 17:03:52 -0500

    Auto complete search added
    added auto-complete search with mapquest api.
    
    need to make it into searchbar, not just li

--- search.js
+++ search.js
@@ -1,5 +1,9 @@
 const current_location_button = document.querySelector('#current_location');
+const searchbar = document.querySelector('#searchbar');
+const resultsList = document.querySelector('#search_results');
 
+
+// Get Current Location Setup
 function success(pos) {
     var crd = pos.coords;
     window.location.replace(`index.html?lat=${crd.latitude}&long=${crd.longitude}`);
@@ -14,6 +18,41 @@ var options = {
 };
 
 
-current_location_button.onclick = function() {
+current_location_button.addEventListener('click', () => {
     navigator.geolocation.getCurrentPosition(success, error, options);
+});
+
+// Searchbar Setup
+
+function getSearchURL(text) {
+    const API_key = 'CjYSjaHyv9CSWQrK7JOeBaLDkcK5ooqi';
+    const url = `http://www.mapquestapi.com/search/v3/prediction?key=${API_key}&limit=5&collection=adminArea,&q=${text}`
+    return url;
 }
+
+
+async function getData(url) { 
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+async function updateSearchResults() {
+    const text = searchbar.value;
+    if (text.length > 1) {
+        const url = getSearchURL(text);
+        const data = await getData(url);
+        console.log(data['results'])
+        var htmlString = "";
+        for (let i=0; i < data['results'].length; i++) {
+            const result = data['results'][i];
+            const resultStr = `<li>${result.displayString}</li>`
+            console.log(resultStr)
+            htmlString += resultStr;
+        }
+        console.log(htmlString)
+        resultsList.innerHTML = htmlString
+    }
+}
+
+searchbar.addEventListener('keyup', updateSearchResults);
